80c7dbbd2fdc053ee12c8b695366b0e2fd4a5839
BurningJump <935646729@qq.com>
2018-02-02 21:25:37 +0800

    登录

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ mongoose.connect('mongodb://localhost:27017/mymovie');
 app.set('views', './views/pages');
 app.set('view engine', 'jade');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.cookieParser());
+app.use(express.session({
+    secret: 'imooc'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.locals.moment = require('moment');
 app.listen(port);
@@ -21,6 +25,8 @@ console.log('express-jade start on port ' + port);
 
 // index page
 app.get('/', function(req, res) {
+    console.log('user in session: ');
+    console.log(req.session.user);
     Movie.fetch(function(err, movies) {
         if(err) {
             console.log(err);
@@ -63,7 +69,28 @@ app.post('user/signin', function(req, res) {
     var name = _user.name;
     var password = _user.password;
 
-    User.findOne({name: name}, )
+    User.findOne({name: name}, function(err, user) {
+        if(err) {
+            console.log(err);
+        }
+
+        if(!user) {
+            return res.redirect('/');
+        }
+
+        user.comparePassword(password, function(err, isMatch) {
+            if(err) {
+                console.log(err);
+            }
+
+            if(isMatch) {
+                req.session.user = user;
+                return res.redirect('/'); 
+            } else {
+                console.log('Password is not matched');
+            }
+        })
+    })
 })
 
 // userList page
